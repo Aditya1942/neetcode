36327bf21a7aa7a7353405cc59dbd21b7257d0bd
Aditya Parmar <58773630+Aditya1942@users.noreply.github.com>
2026-06-21 19:54:37 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        const lenS = s.length;
+        const lenT = t.length;
+        if (lenS !== lenT) return false;
+
+        const mapA = new Map();
+
+        for (let i = 0; i < lenS; i++) {
+            const char = s.at(i);
+            const charCount = mapA.get(char) || 0;
+            mapA.set(char, charCount + 1);
+        }
+        for (let i = 0; i < lenT; i++) {
+            const char = t.at(i);
+            if (!mapA.has(char)) return false;
+            const charCount = mapA.get(char);
+            if (charCount === 1) {
+                mapA.delete(char);
+            } else {
+                mapA.set(char, charCount - 1);
+            }
+        }
+        return !mapA.size;
+    }
+}
