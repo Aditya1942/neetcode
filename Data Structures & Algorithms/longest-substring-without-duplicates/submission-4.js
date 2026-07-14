77406adb29cc6feaa4505ef837669a58e1536fb6
Aditya Parmar <58773630+Aditya1942@users.noreply.github.com>
2026-07-14 22:55:48 +0530

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+  /**
+   * @param {string} s
+   * @return {number}
+   */
+  lengthOfLongestSubstring(s) {
+    let i = 0;
+    let j = 0;
+    const len = s.length;
+    const set = new Set();
+    let max = 0;
+    while (i < len) {
+      const charI = s.at(i);
+      const charJ = s.at(j);
+      if (set.has(charJ)) {
+        i++;
+        set.delete(charI);
+      } else if (j >= len) {
+        set.delete(charI);
+        i++;
+      } else {
+        set.add(charJ);
+        j++;
+      }
+      max = Math.max(max, set.size);
+    }
+    return max;
+  }
+}
