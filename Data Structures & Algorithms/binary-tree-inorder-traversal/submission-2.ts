407d6f071572b9d2f64647f2e440d764f5f7f36b
Aditya Parmar <58773630+Aditya1942@users.noreply.github.com>
2026-09-09 21:25:20 +0530

    Add: binary-tree-inorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-2.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ class TreeNode {
+     constructor(val = 0, left = null, right = null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+ */
+// interface TreeNode {
+//   val: number;
+//   left: TreeNode | null;
+//   right: TreeNode | null;
+// }
+class Solution {
+  /**
+   * @param {TreeNode} root
+   * @return {number[]}
+   */
+
+  result: number[] = [];
+  iter(root: TreeNode | null): void {
+    if (!root) return;
+    if (root.left) this.iter(root.left);
+
+    this.result.push(root.val);
+    if (root.right) this.iter(root.right);
+  }
+
+  inorderTraversal(root: TreeNode | null): number[] {
+    this.iter(root);
+
+    return this.result;
+  }
+}
